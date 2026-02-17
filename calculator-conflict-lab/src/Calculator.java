a18822a0b66710d16ceb88a94b8eefd0b9f32401
abbhosni <binkhalidabdulahv@gmail.com>
2026-02-17 10:25:53 +0400

    Initial calculator setup

---
+++ calculator-conflict-lab/src/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //your code will go here
+
+        scanner.close();
+    }
+}
